dbe9319875dd3bbfd6fcb98e9bb5cae1f43c85ba
Egor Lentarev <egor.lentarev@yandex.ru>
2026-04-30 08:45:34 +1000

    fix(custom cursor)

--- CursorRenderer.cpp
+++ CursorRenderer.cpp
@@ -11,20 +11,24 @@ CursorRenderer::~CursorRenderer() = default;
 
 void CursorRenderer::Render(int cursorSize, int cursorX, int cursorY, HDC hdcMem)
 {
-    // Сохраняем состояние DC
-    int oldRop = SetROP2(hdcMem, R2_XORPEN);
-    HPEN hPen = CreatePen(PS_SOLID, 1, RGB(255, 255, 255));
-    HPEN oldPen = (HPEN)SelectObject(hdcMem, hPen);
-    HBRUSH oldBrush = (HBRUSH)SelectObject(hdcMem, GetStockObject(NULL_BRUSH)); // Прозрачная заливка
+    // outline
+    HPEN hWhitePen = CreatePen(PS_SOLID, 1, RGB(255, 255, 255));
+    HPEN oldPen = (HPEN)SelectObject(hdcMem, hWhitePen);
+    HBRUSH oldBrush = (HBRUSH)SelectObject(hdcMem, GetStockObject(NULL_BRUSH));
 
-    // Рисуем ПРЯМО НА hdcMem (инверсия фона!)
+    // White outline
+    Rectangle(hdcMem, cursorX - 1, cursorY - 1, cursorX + cursorSize + 1, cursorY + cursorSize + 1);
+
+    // main cursor
+    HPEN hBlackPen = CreatePen(PS_SOLID, 1, RGB(0, 0, 0));
+    SelectObject(hdcMem, hBlackPen);
     Rectangle(hdcMem, cursorX, cursorY, cursorX + cursorSize, cursorY + cursorSize);
 
-    // Восстанавливаем состояние
+    // Restoring the state
     SelectObject(hdcMem, oldBrush);
     SelectObject(hdcMem, oldPen);
-    DeleteObject(hPen);
-    SetROP2(hdcMem, oldRop);
+    DeleteObject(hBlackPen);
+    DeleteObject(hWhitePen);
 }
 
 } // namespace baresprite
